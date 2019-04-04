da30cb305546436f84656b52981a9dc3086ebe84
root <root@DESKTOP-2TKMD5S.localdomain>
2019-04-04 08:59:10 +0200

    Unit test

---
+++ BoardgameShop.UnitTests/Services/ProductServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BoardgameShop.Repository;
+using BoardgameShop.Services;
+using BoardgameShop.Models;
+using NUnit.Framework;
+using FakeItEasy;
+using System.Linq;
+
+namespace BoardgameShop.UnitTests.Services
+{
+    class ProductServiceTests
+    {
+        private IProductRepository productRepository;
+        private ProductService productService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.productRepository = A.Fake<IProductRepository>();
+            this.productService = new ProductService(this.productRepository);
+        }
+
+        [Test]
+        public void Get_ReturnsResultFromRepository()
+        {
+            // Arrange
+            var productItem = new Product
+            {
+                ProductId = 2,
+                Name = "Catan",
+                Price = 295,
+                Description = "In Catan players try to be the dominant force on the island of Catan by building settlements, cities, and roads.",
+                Image = "",
+                Category = "strategy"
+            };
+
+            var productItems = new List<Product>
+            {
+                productItem
+            };
+
+            A.CallTo(() => this.productRepository.Get()).Returns(productItems);
+            // Act
+            var result = this.productService.Get().Single();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(productItem));
+        }
+    }
+}
---
+++ BoardgameShop/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoardgameShop.Models;
+
+namespace BoardgameShop.Repository
+{
+    public interface IProductRepository
+    {
+        List<Product> Get();
+        Product Get(int productId);
+        void Add(Product product);
+    }
+}
